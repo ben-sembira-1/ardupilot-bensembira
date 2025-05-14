b1dd76a680d21595485917cd9c45b91e3f41b04c
Ben <ben.sembira.dev@gmail.com>
2025-05-14 09:59:59 +0300

    feat: add new SRx_OPTIONS bitmask parameter with a mavlink2-signing bit

--- libraries/GCS_MAVLink/GCS_Signing.cpp
+++ libraries/GCS_MAVLink/GCS_Signing.cpp
@@ -133,7 +133,8 @@ static bool accept_unsigned_callback(const mavlink_status_t *status, uint32_t ms
 void GCS_MAVLINK::load_signing_key(void)
 {
     struct SigningKey key;
-    if (!signing_key_load(key)) {
+    if (!option_enabled(SROption::MAVLINK2_SIGNING) || !signing_key_load(key))
+    {
         return;
     }
     memcpy(signing.secret_key, key.secret_key, 32);
